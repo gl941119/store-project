987ee51ebb141a7de3688d72aa2de32c9248f841
qian <652344132@qq.com>
2018-11-09 11:15:12 +0800

    2018.11.9

--- src/pages/bindAccount/index.vue
+++ src/pages/bindAccount/index.vue
@@ -100,12 +100,14 @@
                   needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
                   scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
                   success: function (res) {
-                    alert(res)
+                    var s=res+'scanQRCode';
+                    alert(s)
                     var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
                   }
                 });
                 wx.error(function(res){
-                  alert(res)
+                  var s=res+'config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。';
+                  alert(s)
                   // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 
                 });
