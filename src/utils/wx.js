63b928d3c0a46a1d3788d89c2298220b63ccee3e
gl941119 <466922454@qq.com>
2018-11-17 17:26:35 +0800

    test

--- src/utils/wx.js
+++ src/utils/wx.js
@@ -1,61 +1,28 @@
-
 import wx from 'weixin-js-sdk'
-import Vue from 'vue'
-import axios from  'axios'
-
+import request from './request'
 function wxHandle(title, callback) {
-  let config = {
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-  };
-
-  let r = Vue.prototype.$upUrl + 'app/index.php?' + Vue.prototype.$i + '&c=entry&eid=' + Vue.prototype.$eid.eid + '&dom='+Vue.prototype.$eid.dom+'&act=weixinscan&url=' + window.location.href;
-
-  // alert('wx')
-  axios.post(r, null, config)
-    .then((res) => {
-      if (res.data.status) {
-        var d = res.data.data.config;
-        wx.config({
-          debug: false, // 开启调试模式,
-          appId: d.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
-          timestamp: d.timestamp, // 必填，生成签名的时间戳
-          nonceStr: d.nonceStr, // 必填，生成签名的随机串
-          signature: d.signature,// 必填，签名，见附录1
-          jsApiList: ['scanQRCode', 'getLocalImgData', 'downloadImage', 'uploadImage', 'chooseImage', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'updateAppMessageShareData', 'getLocation', 'openLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-        });
-      }
-      }).catch((res) => {
+  request({
+    url:'app/index.php?i=1&c=entry&eid=87&act=weixinscan',
+    data:{
+      url:window.location.href
+    },
+    type:'post'
+  }).then((res) => {
+    if (res.data.status) {
+      var d = res.data.data.config;
+      wx.config({
+        debug: true, // 开启调试模式,
+        appId: d.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
+        timestamp: d.timestamp, // 必填，生成签名的时间戳
+        nonceStr: d.nonceStr, // 必填，生成签名的随机串
+        signature: d.signature,// 必填，签名，见附录1
+        jsApiList: ['scanQRCode', 'getLocalImgData', 'downloadImage', 'uploadImage', 'chooseImage', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'updateAppMessageShareData', 'getLocation', 'openLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      });
+    }
+  }).catch((res) => {
     var ss = res + 'catch请求失败';
     alert(ss)
   });
-
-
-  // alert('wx')
-  // axios.post(r, null, config)
-  //   .then((res) => {
-  //     if (res.data.status) {
-  //       var d = res.data.data.config;
-  //       wx.config({
-  //         debug: false, // 开启调试模式,
-  //         appId: d.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
-  //         timestamp: d.timestamp, // 必填，生成签名的时间戳
-  //         nonceStr: d.nonceStr, // 必填，生成签名的随机串
-  //         signature: d.signature,// 必填，签名，见附录1
-  //         jsApiList: ['scanQRCode', 'getLocalImgData', 'downloadImage', 'uploadImage', 'chooseImage', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'updateAppMessageShareData'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-  //       });
-  //       wx.ready(function () {
-  //         wx[title](callback)
-  //         wx.error(function (res) {
-  //           var s = res + 'config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。';
-  //           alert(s)
-  //           // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-  //         });
-  //       });
-  //     }
-  //   }).catch((res) => {
-  //   var ss = res + 'catch请求失败';
-  //   alert(ss)
-  // });
 }
 
 export default wxHandle
