5e967b7cf9f8af14542577d3bfd40af58c4680f4
qian <652344132@qq.com>
2018-11-27 11:04:32 +0800

    2018.11.27

--- src/pages/setMealHome/page/endorseOneself/endorsementSharing.vue
+++ src/pages/setMealHome/page/endorseOneself/endorsementSharing.vue
@@ -59,7 +59,8 @@
           headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         };
         let r = this.$upUrl + 'app/index.php?' + this.$i + '&c=entry&eid=' + this.$eid.eid + '&dom='+this.$eid.dom+'&act=weixinscan&url=' + betUrl;
-
+alert(r)
+        alert(ul.split('#')[0])
         axios.post(r, null, config)
           .then((res) => {
             if (res.data.status) {
